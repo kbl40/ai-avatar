a1ac289a88f8fe26302d27b8828195ca62f1ad27
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2023-01-23 21:15:40 -0500

    logic added

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,13 +1,15 @@
 const generateAction = async (req, res) => {
     console.log('Received request')
+
     const input = JSON.parse(req.body).input;
 
     const response = await fetch(
-        `https://api-inference.huggingface.co/models/KyleLackinger/kylack`,
+        `https://api-inference.huggingface.co/models/KyleLackinger/sd-1-5-lackinger`,
         {
             headers: {
                 Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
                 'Content-Type': 'application/json',
+                'x-use-cache': 'false',
             },
             method: 'POST',
             body: JSON.stringify({
@@ -16,17 +18,28 @@ const generateAction = async (req, res) => {
         }
     );
 
+    const bufferToBase64 = (buffer: ArrayBuffer) => {
+        let arr = new Uint8Array(buffer);
+        const base64 = btoa(
+            arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+        );
+        return `data:image/png;base64,${base64}`;
+    };
+
     // Check for different statuses to send proper payload
     if (response.ok) {
         const buffer = await response.arrayBuffer();
-        res.status(200).json({ image: buffer });
-    } else if (response.status == 503) {
+        const base64 = bufferToBase64(buffer);
+        res.status(200).json({ image: base64 });
+    } else if (response.status === 503) {
         const json = await response.json();
         res.status(503).json(json);
     } else {
         const json = await response.json();
         res.status(response.status).json({ error: response.statusText });
     }
+
+    
 };
 
 export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,52 @@ const Home = () => {
   const maxRetries = 20;
   // Create state property
   const [input, setInput] = useState('');
+
+  // need useState for Artist, Vibe, Medium, and Descriptors. these will be concatenated to form the input
+  const [artistInput, setArtistInput] = useState('')
+  const [mediumInput, setMediumInput] = useState('')
+  const [vibeInput, setVibeInput] = useState('')
+  const [descriptorInput, setDescriptorInput] = useState('')
+
   const [img, setImg] = useState('');
   const [retry, setRetry] = useState(0);
   const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
 
   const onChange = (event) => {
     setInput(event.target.value)
   }
 
+  // Capture and set the additional input fields
+  const onArtistChange = (event) => {
+    setArtistInput(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const onMediumChange = (event) => {
+    console.log(event.target.value)
+    setMediumInput(event.target.value)
+  }
+
+  const onVibeChange = (event) => {
+    console.log(event.target.value)
+    setVibeInput(event.target.value)
+  }
+
+  const onDescriptorChange = (event) => {
+    console.log(event.target.value)
+    setDescriptorInput(event.target.value) 
+  }
+
   const generateAction = async () => {
     console.log('Generating...')
 
+    // check to make sure no double click
+    if (isGenerating && retry === 0) return;
+
+    setIsGenerating(true);
+    
     if (retry > 0) {
       setRetryCount((prevState) => {
         if (prevState === 0) {
@@ -30,28 +65,40 @@ const Home = () => {
       setRetry(0)
     }
 
+    // Update this to concatenate all inputs 
+    const concatenateInputs = `${mediumInput} intricate character portrait of klack as d&d character by ${artistInput}, ${descriptorInput}, ${vibeInput}, ${input}`
+    const finalInput = concatenateInputs.replace(/kyle/gi, 'klack');
+
     // Add the fetch request
-    const response = await fetch('api/generate', {
+    const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'image/jpeg',
       },
-      body: JSON.stringify({ input }),
+      body: JSON.stringify({ input: finalInput }),
     });
 
     const data = await response.json();
 
-    if (response.status == 503) {
+    if (response.status === 503) {
       setRetry(data.estimated_time);
       return;
     }
 
     if (!response.ok) {
       console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
       return;
     }
 
+    setFinalPrompt(finalInput);
+    setInput('');
+    setArtistInput('');
+    setMediumInput('');
+    setVibeInput('');
+    setDescriptorInput('');
     setImg(data.image);
+    setIsGenerating(false);
   };
 
 const sleep = (ms) => {
@@ -85,27 +132,56 @@ const sleep = (ms) => {
   return (
     <div className="root">
       <Head>
-        <title>Old school picture generator | buildspace</title>
+        <title>D&D picture generator | buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Old school picture generator</h1>
+            <h1>D&D picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Make some old school looking pictures of me...or do whatever you want. idc, just use "kyLack" as my name in the prompt</h2>
+            <h2>Make some D&D portraits of me. Fill in the blanks, get a pic - you get the idea.</h2>
           </div>
           <div className='prompt-container'>
-            <input className='prompt-box' value={input} onChange={onChange}/>
+            <div className='sub-prompt-container'>
+              <h2 className='sub-prompt-title'>Artist</h2>
+              <input className='prompt-box' value={artistInput} onChange={onArtistChange} />
+            </div>
+            <div className='sub-prompt-container'>
+              <h2 className='sub-prompt-title'>Medium</h2>
+              <input className='prompt-box' value={mediumInput} onChange={onMediumChange} />
+            </div>
+            <div className='sub-prompt-container'>
+              <h2 className='sub-prompt-title'>Vibe</h2>
+              <input className='prompt-box' value={vibeInput} onChange={onVibeChange} />
+            </div>
+            <div className='sub-prompt-container'>
+              <h2 className='sub-prompt-title'>Descriptor</h2>
+              <input className='prompt-box' value={descriptorInput} onChange={onDescriptorChange} />
+            </div>
+            <div className='sub-prompt-container extra-details'>
+              <h2 className='sub-prompt-title'>Extra details</h2>
+              <input className='prompt-box' value={input} onChange={onChange}/>
+            </div>
             <div className='prompt-buttons'>
-              <a className='generate-button' onClick={generateAction}>
+              <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={generateAction}>
                 <div className='generate'>
-                  <p>Generate</p>
+                  {isGenerating ? (
+                    <span className='loader'></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
                 </div>
               </a>
             </div>
           </div>
         </div>
+        {img && (
+          <div className='output-content'>
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
